26ec87b7a45c7d148dd05565d2f22f31359f4ab7
Robert Kipkoech <71884172+Robaa2168@users.noreply.github.com>
2023-04-15 21:42:07 +0300

    Update Login.js

--- src/components/signin/Login.js
+++ src/components/signin/Login.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
-import styles from "./Login.css";
+import styles from "./Login.module.css";
 import {
   Card,
   VStack,
@@ -17,13 +17,14 @@ import {
   useBreakpointValue,
 } from "@chakra-ui/react";
 
+
 const Login = () => {
   const formik = useFormik({
     initialValues: {
       email: "",
       password: "",
     },
-    onSubmit: (values) => {},
+    onSubmit: (values) => { },
     validationSchema: Yup.object({
       email: Yup.string()
         .email("Invalid email address")
@@ -42,10 +43,19 @@ const Login = () => {
 
   return (
     <>
-      <Card w={cardWidth} ml="auto" mr="auto" my={{ base: "10%", md: "5%" }}>
+      <Card
+        w={cardWidth}
+        ml="auto"
+        mr="auto"
+        my={{ base: "10%", md: "5%" }}
+        bg="white"
+        boxShadow="lg"
+        borderRadius="lg"
+      >
         <div>
           <Center>
-            <Avatar name="logo" src={Image} m="5%" mb="5%" />
+            <Avatar bg="blue.500" color="white" m="5%" mb="5%">
+            </Avatar>
           </Center>
           <form onSubmit={formik.handleSubmit}>
             <VStack spacing={4} maxW="70%" mx="auto">
@@ -59,6 +69,7 @@ const Login = () => {
                   type="email"
                   name="email"
                   placeholder="Email Address"
+                  borderColor="gray.400"
                   {...formik.getFieldProps("email")}
                 />
 
@@ -74,22 +85,23 @@ const Login = () => {
                   type="password"
                   name="password"
                   placeholder="Enter Your Password"
+                  borderColor="gray.400"
                   {...formik.getFieldProps("password")}
                 />
 
                 <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
-                <a href="./">
-                  <Heading as="h4" size="md" my="3" className="text-primary">
+                <Link href="./" color="blue.500">
+                  <Heading as="h4" size="md" my="3">
                     Fogot password?
                   </Heading>
-                </a>
+                </Link>
               </FormControl>
 
               <Button
                 type="submit"
                 colorScheme="blue"
                 width="50%"
-                className="rounded-pill"
+                borderRadius="full"
               >
                 <Link href="/">Login</Link>
               </Button>
@@ -97,18 +109,23 @@ const Login = () => {
           </form>
 
           <Center>
-            <a href="./">
-              <Heading as="h4" size="md" my="3" className="text-primary">
+            <Link href="./" color="blue.500">
+              <Heading as="h4" size="md" my="3">
                 Try another way
               </Heading>
-            </a>
+            </Link>
           </Center>
-     
+
           <VStack spacing={4} maxW="70%" mx="auto" mb="3%">
             <Link
-              href="/signup"   className="btn btn-outline-primary rounded-pill hover-light"
+              href="/signup"
+              className="btn btn-outline-primary rounded-pill hover-light"
               w="50%"
               p="8px"
+              color="blue.500"
+              borderColor="blue.500"
+              borderWidth="1px"
+              borderRadius="full"
             >
               Sign Up
             </Link>
@@ -150,12 +167,12 @@ const Login = () => {
         style={{ marginLeft: "auto", marginRight: "auto" }}
         className="text-center"
       >
-        <Text mt="40" fontSize="xl">
+        <Text mt="40" fontSize="xl" color="gray.600">
           ©2023 PayPal Privacy Legal Contact Feedback
         </Text>
       </div>
     </>
-    );
-  };
-  
-  export default Login;
+  );
+};
+
+export default Login;
