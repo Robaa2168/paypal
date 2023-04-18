f32dee7a1b9d98673f055876d7d8d2223d2cc9be
Robert Kipkoech <71884172+Robaa2168@users.noreply.github.com>
2023-04-18 19:15:39 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import Dashboard from "./components/Dashboard";
+import Header from './components/Header';
 import Step1 from "./components/signup/Step1";
 import Step4 from "./components/signup/Step4";
 import Step3 from "./components/signup/Step3";
@@ -5,7 +8,6 @@ import Step2 from "./components/signup/Step2";
 import Login from "./components/signin/Login";
 import SendMoney from "./components/SendMoney";
 import RequestMoney from "./components/RequestMoney";
-import Settings from './components/settings/Settings';
 import Activity from "./components/Activity";
 import Signup from "./components/signup/Signup";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -26,7 +28,6 @@ const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/send-money" element={<SendMoney />} />
           <Route path="/send-money/preview" element={<SendPreview />} />
-           <Route path="/settings/security" element={<Settings />} />
           <Route path="/request-money" element={<RequestMoney />} />
           <Route path="request-money/preview" element={<RequestPreview />} />
           <Route path="/activity" element={<Activity />} />
