5c0de0a896c98ff5d5fd8af7c477f55d3fa1bd12
Leestan360 <stanleymayore2@gmail.com>
2023-04-20 18:59:17 +0300

    Add currency modal

--- src/components/wallet1/Wallet1.jsx
+++ src/components/wallet1/Wallet1.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Wallet1.css";
 import { Link } from "react-router-dom";
 import { AiFillCreditCard } from "react-icons/ai";
+import { CiMenuKebab } from "react-icons/ci";
 import PaymentPreferencesModal from "./modals/PaymentPreferencesModal";
 import SetPreferredModal from "./modals/SetPreferredModal";
 import AddCurrencyModal from "./modals/AddCurrencyModal";
@@ -11,6 +12,7 @@ function Wallet1() {
   const [modalVisible1, setModalVisible1] = useState(false);
   const [modalVisible2, setModalVisible2] = useState(false);
   const [modalVisible3, setModalVisible3] = useState(false);
+  const [popupVisible, setPopupVisible] = useState(false);
 
   function toggleModal() {
     setModalVisible(!modalVisible);
@@ -27,6 +29,11 @@ function Wallet1() {
   function toggleModal3() {
     setModalVisible3(!modalVisible3);
   }
+
+  function togglePopup() {
+    setPopupVisible(!popupVisible);
+  }
+
   return (
     <div className="walletMain">
       <div className="walletContainer">
@@ -61,14 +68,58 @@ function Wallet1() {
           <p className="walletContainerDiv2Bal">$0.00</p>
           <p className="walletContainerDiv2P2">Available</p>
           <button className="walletContainerDiv2Btn">Send payments</button>
+
+          <div className="walletContainingCurrency">
+            <div className="walletContainingCurrencyDiv">
+              <img
+                src="/european.png"
+                alt="EURO"
+                className="walletContainingCurrencyImg"
+              />
+              <div className="walletContainingCurrencyDiv1">
+                <p className="walletContainingCurrencyP">EURO</p>
+                <p className="walletContainingCurrencyP1">PRIMARY</p>
+              </div>
+            </div>
+            <p className="walletContainingCurrencyBal">$0.00</p>
+          </div>
+
+          <div className="walletContainingCurrency">
+            <div className="walletContainingCurrencyDiv">
+              <img
+                src="/european.png"
+                alt="EURO"
+                className="walletContainingCurrencyImg"
+              />
+              <p className="walletContainingCurrencyP2">EURO</p>
+            </div>
+            <div className="div9">
+              <div className="div10">
+                <p className="walletContainingCurrencyBal">$0.00</p>
+                <p className="walletContainingCurrencyBal">$0.00 in EURO</p>
+              </div>
+              <CiMenuKebab className="div9Icon" onClick={togglePopup} />
+              {popupVisible && (
+                <div className="popupMenu">
+                  <Link className="popupLink">Link 1</Link>
+                  <Link className="popupLink">Link 2</Link>
+                </div>
+              )}
+            </div>
+          </div>
+
           <div className="addCurrency">
-            <Link className="addCurrencyLink1" onClick={toggleModal2}>Add a currency</Link>
+            <Link className="addCurrencyLink1" onClick={toggleModal2}>
+              Add a currency
+            </Link>
             <Link className="addCurrencyLink2">Currency Calculator</Link>
           </div>
 
           <div className="preferred">
             <p className="preferredOnline">Preferred when paying online</p>
-            <Link className="setPreferred" onClick={toggleModal1}>Set as preferred</Link>
+            <Link className="setPreferred" onClick={toggleModal1}>
+              Set as preferred
+            </Link>
             <p className="preferredP1">
               We'll use your available balance when you shop online or send
               money for goods and services.
@@ -82,7 +133,10 @@ function Wallet1() {
             </Link>
           </div>
         </div>
-        <PaymentPreferencesModal isVisible={modalVisible} onClose={toggleModal} />
+        <PaymentPreferencesModal
+          isVisible={modalVisible}
+          onClose={toggleModal}
+        />
         <SetPreferredModal isVisible={modalVisible1} onClose={toggleModal1} />
         <AddCurrencyModal isVisible={modalVisible2} onClose={toggleModal2} />
       </div>
